03ef301e8f58251332181e104b6f48ec047e9d7c
Adnangad <gardobuyaadnan@gmail.com>
2023-11-28 03:07:23 -0800

    task-6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,6 +4,6 @@ for i in range(9):
         if i == x or (str(i) + str(x) == "10"):
             continue
         if (str(i) + str(x) != "89"):
-            print(str(i) + str(x) + ", ", end='')
+            print("{}, ".format(str(i) + str(x)), end='')
         else:
-            print(str(i) + str(x))
+            print("{}".format(str(i) + str(x)))
