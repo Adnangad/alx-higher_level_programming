66205dc2d3de4cbc9b5b42ff88b80418840aef81
Adnangad <gardobuyaadnan@gmail.com>
2023-11-28 03:18:05 -0800

    task-6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(9):
     for x in range(10):
-        if i == x or (str(x) == "0"):
+        if i == x or (str(x) == "0") or (str(i) + str(x) > str(x) + str(i)):
             continue
         if (str(i) + str(x) != "89"):
             print("{}, ".format(str(i) + str(x)), end='')
