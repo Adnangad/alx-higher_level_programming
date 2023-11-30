ced5a193e577c677447c67b5fb95db76ec030aee
Adnangad <gardobuyaadnan@gmail.com>
2023-11-30 05:28:29 -0800

    task-4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import hidden_4
-    print("my_secret_santa")
+    import hidden_4 as hd
+    for i in dir(hd):
+        if i[0:2] != "__":
+            print("{}".format(i))
