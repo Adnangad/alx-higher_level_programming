f381255b9000d2b160f9afcfa17a0617b3b4e233
Adnangad <gardobuyaadnan@gmail.com>
2023-11-30 02:45:58 -0800

    task-2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import argparse
+parser = argparse.ArgumentParser(description='Shows the number of arg and displays it')
+parser.add_argument('string', nargs='*')
+args = parser.parse_args()
+num = len(args.string)
+if num <  1:
+    print("{} arguments.".format(num))
+elif num > 1:
+    print("{} arguments:".format(num))
+else:
+    print("{} argument:".format(num))
+for i in range(1, num + 1):
+    print("{}: {}".format(i, args.string[i - 1]))
