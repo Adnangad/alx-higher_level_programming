18eb822d553b0e2ee8f8be6c88a671a8852c5b3c
Adnangad <gardobuyaadnan@gmail.com>
2023-11-30 02:57:25 -0800

    task-2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
     import argparse
-    parser = argparse.ArgumentParser(description='Shows the number of arg and displays it')
+    parser = argparse.ArgumentParser(description='displays arg')
     parser.add_argument('string', nargs='*')
     args = parser.parse_args()
     num = len(args.string)
-    if num <  1:
+    if num < 1:
         print("{} arguments.".format(num))
     elif num > 1:
         print("{} arguments:".format(num))
