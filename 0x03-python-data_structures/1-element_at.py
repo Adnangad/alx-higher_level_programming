95b777cf54aca4fab51f8a07f87396653e78e5a8
Adnangad <gardobuyaadnan@gmail.com>
2023-12-01 06:13:04 -0800

    task-1

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    pos = my_list.pop(idx)
+    leng = len(my_list)
+    if idx < 0:
+        return None
+    if idx > leng:
+        return None
+    else:
+        return pos
