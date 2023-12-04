54389b8ce84fdff1b18287f38862ff051453a7f6
Adnangad <gardobuyaadnan@gmail.com>
2023-12-04 05:31:32 -0800

    task-7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -5,6 +5,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
     if len_a < 2:
         tuple_a += (0,) * (2 - len_a)
     if len_b < 2:
-        tuple_b += (0,) * (2- len_b)
+        tuple_b += (0,) * (2 - len_b)
     rez = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
     return rez
