157713d1fb711b614c3347e03b320c20c4964f4f
Adnangad <gardobuyaadnan@gmail.com>
2023-12-04 04:16:23 -0800

    task-6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,4 +5,4 @@ def print_matrix_integer(matrix=[[]]):
             if j != 0:
                 print(" ", end='')
             print("{:d}".format(val), end=' ')
-        print()
+        print("$")
