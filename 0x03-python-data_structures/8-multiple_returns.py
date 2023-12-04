1539f6b03ce4730dc5a2db598a5aec354ee58804
Adnangad <gardobuyaadnan@gmail.com>
2023-12-04 05:38:54 -0800

    task-8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence == '':
-        sentence[0] = None
+        return (0, None)
     tup = (len(sentence), sentence[0])
     return tup
