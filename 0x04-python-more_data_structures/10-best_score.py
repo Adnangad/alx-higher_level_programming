a46c48b96f095fbc8a363bc0d9e926fdd56c92ed
Adnangad <gardobuyaadnan@gmail.com>
2023-12-06 10:11:19 -0800

    task-10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,16 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    ls = []
-    for i, j in a_dictionary.items():
-        ls.append(j)
-    k = len(ls) - 1
-    while k >= 0:
-        l = 0
-        while l < k:
-            if ls[l] > ls[l + 1]:
-                temp = ls[l]
-                ls[l] = ls[l + 1]
-                ls[l + 1] = temp
-            l += 1
-        k -= 1
-    return ls[-1]
+    if not a_dictionary:
+        return None
+    return max(a_dictionary, key=a_dictionary.get)
