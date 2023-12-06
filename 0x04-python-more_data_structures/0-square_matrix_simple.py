a2ae54ef8f65fe0760df586be954b0b48bdff4fb
Adnangad <gardobuyaadnan@gmail.com>
2023-12-06 07:14:54 -0800

    task-2

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    for x in matrix:
-        for y in x:
-            return (x**2, y**2)
+    rez = list(map(lambda x, y: x*y, matrix))
+    return rez
--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,13 +1,4 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    rez = 0
-    i = 0
-    new = len(my_list) - 1
-    while i  < new:
-        if my_list[i] == my_list[i + 1]:
-            my_list.pop(i)
-        else:
-            i += 1
-    for k in my_list:
-        rez += k
-    return rez
+    rez = set(my_list)
+    return sum(rez)
