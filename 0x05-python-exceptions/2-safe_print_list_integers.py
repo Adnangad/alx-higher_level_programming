f96a9fd6de018eeb1793e7e8eca1c5e4e211e153
Adnangad <gardobuyaadnan@gmail.com>
2023-12-18 04:48:16 -0800

    task-5

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    rez = 0
+    try:
+        for i in range(x):
+            try:
+                if isinstance(my_list[i], int):
+                    print("{:d}".format(my_list[i]), end='')
+                    rez += 1
+            except (ValueError, TypeError):
+                pass
+        print()
+    except (TypeError, ValueError):
+        return rez
