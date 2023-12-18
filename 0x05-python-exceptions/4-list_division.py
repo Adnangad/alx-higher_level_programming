9c9b7a4e20e6da3a4ac1b68d186a572ec205d945
Adnangad <gardobuyaadnan@gmail.com>
2023-12-18 10:16:11 -0800

    task-4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -16,5 +16,7 @@ def list_division(my_list_1, my_list_2, list_length):
                 print("out of range")
                 new.append(0)
     except TypeError:
-        pass
-    return new
+        print("iterable")
+        return []
+    else:
+        return new
