bc251a0f86addc430583b5ad58997aa3c4b96c6f
Adnangad <gardobuyaadnan@gmail.com>
2023-12-19 06:46:39 -0800

    task-3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Defines a SizeError class."""
+
+
+class SizeError(Exception):
+    """Represents an empty SizeError."""
+    pass
+
+
+"""Defines a Square class."""
+
+
+class Square:
+    """Represents a square with a specific size."""
+    def __init__(self, size=0):
+        """Initializes the Square instance.
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
+        if not isinstance(size, int):
+            raise SizeError("size must be an integer")
+        if size < 0:
+            raise SizeError("size must be >= 0")
+    def area(self):
+        """Return the area."""
+        return self.__size * self.__size
