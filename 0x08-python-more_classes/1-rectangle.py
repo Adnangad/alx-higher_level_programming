4066b104a0908188fe69dfe7d5c5b212b9968b25
Adnangad <gardobuyaadnan@gmail.com>
2024-01-04 03:45:37 -0800

    1-rectangle with values

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+This module creates a class.
+"""
+
+
+class Rectangle:
+    """
+    Initializes the class.
+    """
+
+    def __init__(self, width=0, height=0):
+        """Sets the var."""
+        self.__width = width
+        self.__height = height
+    @property
+    def width(self):
+        """Gets the width."""
+        return self.__width
+    @width.setter
+    def width(self, value):
+        """Sets the width."""
+        self.__width = value
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+    @property
+    def height(self):
+        """Gets the height."""
+        return self.__height
+    @height.setter
+    def height(self, value):
+        """Sets height."""
+        self.__height = value
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
