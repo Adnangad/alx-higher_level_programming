7d8c533c7d165fed85ca3c5cb17ac45817d96533
Adnangad <gardobuyaadnan@gmail.com>
2024-01-04 06:44:33 -0800

    task-7

---
+++ 0x08-python-more_classes/7-rectangle.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+"""
+This module creates a class.
+"""
+
+
+class Rectangle:
+    """
+    Initializes the class.
+    """
+    number_of_instances = 0
+    print_symbol = '#'
+
+    def __init__(self, width=0, height=0):
+        """Sets the var."""
+        self.__height = height
+        self.__width = width
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        """Gets the width."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Sets the width."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Gets the height."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Sets height."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """returns the area."""
+        return self.width * self.height
+
+    def perimeter(self):
+        """Returns perimeter."""
+        if self.width == 0 or self.height == 0:
+            return 0
+        return self.width + self.width + self.height + self.height
+
+    def __str__(self):
+        x = ""
+        if self.width == 0 or self.height == 0:
+            return x
+        for i in range(self.height):
+            x += str(self.print_symbol) * self.width + "\n"
+        return x[:-1]
+
+    def __repr__(self):
+        """Return String rep."""
+        if self.width == 0 or self.height == 0:
+            return ""
+        return f"Rectangle({self.__width}, {self.__height})"
+
+    def __del__(self):
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
