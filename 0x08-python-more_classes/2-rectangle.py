ab1d17c273afa90f6e472c3faed081028a7d280b
Adnangad <gardobuyaadnan@gmail.com>
2024-01-04 04:22:14 -0800

    task-2

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+This module creates a class.
+"""
+
+
+class Rectangle:
+    """
+    Initializes the class.
+    """
+    def __init__(self, width=0, height=0):
+        """Sets the var."""
+        self.__height = height
+        self.__width = width
+
+    @property
+    def width(self):
+        """Gets the width."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Sets the width."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Gets the height."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Sets height."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """returns the area."""
+        return self.width * self.height
+
+    def perimeter(self):
+        """Returns perimeter."""
+        if self.width == 0 or self.height == 0:
+            return 0
+        return self.width + self.width + self.height + self.height 
