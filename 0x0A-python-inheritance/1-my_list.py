cbf50d39ba7c8e8273821864d92de9b8748afb65
Adnangad <gardobuyaadnan@gmail.com>
2024-01-08 04:08:25 -0800

    task-2

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This is a module that contains a subclass.
+"""
+
+
+class MyList(list):
+    """
+    This is a subclass that inherits from a list.
+    """
+    def append(self):
+    def print_sorted(self):
+        print(super().sort())
---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This module checks if an object is an instance of a class.
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Checks if is instance.
+
+    Args:
+    obj (class):the object to be checked
+    a_class (class):the measure
+
+    Return:
+    True if isinstance
+    False if not.
+    """
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
