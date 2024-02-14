b171e5d3c5119bb0991d9bb82254f0e94f16cc73
Adnangad <gardobuyaadnan@gmail.com>
2024-02-14 08:07:53 -0800

    8 and 9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- uses join to combine 2 rows of a table
-SELECT cities.id, cities.name FROM cities LEFT JOIN states ON states.id = cities.state_id ORDER BY cities.id ASC;
+SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states ON states.id = cities.state_id ORDER BY cities.id ASC;
