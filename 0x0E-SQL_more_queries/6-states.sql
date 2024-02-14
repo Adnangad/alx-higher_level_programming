3587cc860dbfe5dc0896cbcffa78acf6d4e8e0dd
Adnangad <gardobuyaadnan@gmail.com>
2024-02-14 07:03:01 -0800

    6-database plus table

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,3 @@
 -- creates a database, atable in that database and a primary key in the table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
--- switches to the database
-USE hbtn_0d_usa;
--- creates a table
-CREATE TABLE IF NOT EXISTS state(id INT AUTO_INCREMENT PRIMARY KEY , name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (id INT UNIQUE NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY(id));
