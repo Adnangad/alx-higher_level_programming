05591418066b96d09439ba996714dcf0c3d42427
Adnangad <gardobuyaadnan@gmail.com>
2024-03-12 05:04:12 -0700

    4-added double and rotate methods

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -20,4 +20,15 @@ module.exports = class Rectangle {
       x++;
     }
   }
+
+  rotate () {
+    let temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width = 2 * this.width;
+    this.height = 2 * this.height;
+  }
 };
