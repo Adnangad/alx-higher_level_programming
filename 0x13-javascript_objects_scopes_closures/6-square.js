a54053cebec9d16f74eb03f7c7acf54e6911840a
Adnangad <gardobuyaadnan@gmail.com>
2024-03-12 05:58:37 -0700

    6-inheriting methods

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+  charPrint (c) {
+    if (typeof(c) === 'undefined') {
+      this.print();
+    } else {
+      let x = 0;
+      while (x < this.height) {
+        let y = 0;
+        while (y < this.width) {
+          process.stdout.write(c);
+          y++;
+	}
+        process.stdout.write('\n');
+        x++;
+      }
+    }
+  }
+};
