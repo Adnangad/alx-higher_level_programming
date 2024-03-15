5ec5a1d607ca537ca54d642ce6ce6348f3d25956
Adnangad <gardobuyaadnan@gmail.com>
2024-03-15 04:08:14 -0700

    3-safe from injenction

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+lists the states in a table.
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+            )
+    cur = db.cursor()
+    query = "SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY states.id ASC"
+    cur.execute(query, (sys.argv[2],))
+    rez = cur.fetchall()
+    for i in rez:
+        print(f"{i}")
+    cur.close()
+    db.close()
