b6df0273f9d5acab4daf2ec016ead72b037b8c72
Adnangad <gardobuyaadnan@gmail.com>
2024-03-15 01:26:18 -0700

    0-first task

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,16 +3,12 @@ import MySQLdb
 import sys
 if __name__ == "__main__":
     try:
-        username, password, database = sys.argv[1:]
-    except ValuError:
-        sys.exit(1)
-    try:
         connection = MySQLdb.connect(
                 host='localhost',
                 port=3306,
-                user=username,
-                passwd=password,
-                db=database
+                user=sys.argv[1],
+                passwd=sys.argv[2],
+                db=sys.argv[3]
                 )
         cursor = connection.cursor()
         cursor.execute('SELECT * FROM states ORDER BY states.id ASC')
