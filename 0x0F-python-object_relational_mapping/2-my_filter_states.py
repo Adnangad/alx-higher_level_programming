ffda6971728c0d77c3cdcdaa965102ec566075a6
Adnangad <gardobuyaadnan@gmail.com>
2024-03-15 01:49:35 -0700

    2-filter by name

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+lists the states in a table.
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+            )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY states.id ASC", (sys.argv[4],))
+    rez = cur.fetchall()
+    for i in rez:
+        print(f"{i}")
+    cur.close()
+    db.close()
