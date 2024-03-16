b798dbc16b60b5ba5234675d5b9d4660de5a44e2
Adnangad <gardobuyaadnan@gmail.com>
2024-03-16 01:54:47 -0700

    12-updated state

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This module lists the table.
+"""
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+import sys
+
+if __name__ == "__main__":
+    engine = create_engine('mysql://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    rez = session.query(State).filter(State.id==2).first()
+    rez.name = 'New Mexico'
+    session.commit()
+    session.close()
