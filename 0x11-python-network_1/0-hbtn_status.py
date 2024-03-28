a8bd67eb7db6897686d268f1706452fd8ebdb17d
Adnangad <gardobuyaadnan@gmail.com>
2024-03-28 06:55:50 -0700

    0-urllib

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module fetches data from an url."""
+import urllib.request
+
+
+if __name__ == '__main__':
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        response.read()
