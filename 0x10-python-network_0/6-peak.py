b93080bc803845f018de38e574cf65dfdc118cd1
Adnangad <gardobuyaadnan@gmail.com>
2024-03-30 04:54:38 -0700

    6-peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,8 +3,10 @@
 
 
 def find_peak(list_of_integers):
-    if not list_of_integers:
+    if len(list_of_integers) == 0 or list_of_integers is None:
         return None
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]
     i, j = 0, len(list_of_integers) - 1
 
     while j > i:
