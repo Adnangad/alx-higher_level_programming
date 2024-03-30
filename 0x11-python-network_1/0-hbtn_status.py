4146ddd49bb45742fa422940ddad4bc74511c2a9
Adnangad <gardobuyaadnan@gmail.com>
2024-03-30 05:07:59 -0700

    5-header

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,7 +6,7 @@ import urllib.request
 if __name__ == '__main__':
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         rez = response.read()
-        print("Body response:")
-        print(f'\t- type: {type(rez)}')
-        print(f'\t- content: {rez}')
-        print(f'\t- utf content: {rez.decode("utf-8")}')
+    print("Body response:")
+    print(f'\t- type: {type(rez)}')
+    print(f'\t- content: {rez}')
+    print(f'\t- utf content: {rez.decode("utf-8")}')
