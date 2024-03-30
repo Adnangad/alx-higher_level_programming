f2f695dc095b08246f05605c3c8da689b11a3d43
Adnangad <gardobuyaadnan@gmail.com>
2024-03-30 05:23:39 -0700

    5-header

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,9 +2,10 @@
 """Module displays request using request package"""
 import requests
 
+
 if __name__ == '__main__':
     rez = requests.get('https://alx-intranet.hbtn.io/status')
     final = rez.content
-    print("Body response:$")
-    print(f'\t- type: {type(final)}$')
-    print(f'\t- content: {final.decode("utf-8")}$')
+    print("Body response:")
+    print(f'\t- type: {type(final)}')
+    print(f'\t- content: {final.decode("utf-8")}')
