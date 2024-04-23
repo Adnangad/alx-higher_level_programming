04c5e67cd1611c77f4c3738514d2bf4873893e4d
Adnangad <gardobuyaadnan@gmail.com>
2024-04-23 10:10:01 -0700

    6

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
 const url = process.argv[2];
 const request = require('request');
-request.get(`${url}`, function (error, response, body) {
-  if (error) {
-    console.error(error);
-  }
+request(`${url}`, function (error, response, body) {
   console.log('code: ', response.statusCode);
 });
